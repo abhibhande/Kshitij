dab2f82b3ec828e048a47487ec572355329facc6
DELL <ahiraomrudul@gmail.com>
2025-01-18 16:20:33 +0530

    Updated till the taskmanager

--- src/components/Navbar/Tabs/page.tsx
+++ src/components/Navbar/Tabs/page.tsx
@@ -4,32 +4,32 @@ import Friends from "@/components/friend_components/page";
 import { Task } from "@/components/Task";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const [data, updateData] = useState<any>([]);
+// const [data, updateData] = useState<any>([]);
 
-  useEffect(() => {
-    updateData([
-      {
-        id: 1,
-        title: "Sample test note completion",
-        dueDate: "10/01/2025:10:00",
-        desc: "Complete the sample test note for review and submission."
-      },
-      {
-        id: 2,
-        title: "Big task completion pending",
-        dueDate: "10/01/2025:10:00",
-        desc: "Finish the major task that has been marked as pending for the project deadline."
-      },
-      {
-        id: 3,
-        title: "Sample test note completion",
-        dueDate: "10/01/2025:10:00",
-        desc: "Ensure the sample test note is fully prepared for presentation."
-      }
-    ]);
-  }, []);
+//   useEffect(() => {
+//     updateData([
+//       {
+//         id: 1,
+//         title: "Sample test note completion",
+//         dueDate: "10/01/2025:10:00",
+//         desc: "Complete the sample test note for review and submission."
+//       },
+//       {
+//         id: 2,
+//         title: "Big task completion pending",
+//         dueDate: "10/01/2025:10:00",
+//         desc: "Finish the major task that has been marked as pending for the project deadline."
+//       },
+//       {
+//         id: 3,
+//         title: "Sample test note completion",
+//         dueDate: "10/01/2025:10:00",
+//         desc: "Ensure the sample test note is fully prepared for presentation."
+//       }
+//     ]);
+//   }, []);
 
 export default function TopTabs() {
   return (
@@ -45,7 +45,7 @@ export default function TopTabs() {
                 <CardTitle>Tasks</CardTitle>
             </CardHeader>
             <CardContent>
-                <Task id={undefined} title={"abc"} dueDate={""} completeTask={undefined}/>
+                <Task id={1} title={"abc"} dueDate={""} completeTask={undefined}/>
             </CardContent>
             </Card>
       </TabsContent>
